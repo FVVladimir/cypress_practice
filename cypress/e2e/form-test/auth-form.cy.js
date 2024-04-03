9bf31626e287c6d4409fb01592143b7747a43e7e
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-04-03 22:00:48 +0300

    task is done

---
+++ cypress/e2e/form-test/auth-form.cy.js
@@ -0,0 +1,25 @@
+
+
+describe('pass form testing', () => {
+  it('right login wrong pass, check error massage', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get("#username").type("tomsmith");
+    cy.get("#password").type("123456");
+    cy.get("button[type = 'submit']").click();
+    cy.get("#flash").should("have.text", '\n            Your password is invalid!\n            ×\n          ');
+  });
+  
+  it('wrong login any password, check error massage', () => {
+    cy.visit('https://the-internet.herokuapp.com/login',
+    {
+      auth:{
+        username:"johnsmith",
+        password:"12345"
+      },
+      // failOnStatusCode: false
+     
+    });    
+    cy.get("button[type = 'submit']").click();
+    cy.get("#flash").should("have.text", '\n            Your username is invalid!\n            ×\n          ');
+  });
+});
