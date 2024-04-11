6df9379e0ec18e53576275b5148de91990bcaaaf
Volodymyr Fedchenko <emirwtf@gmail.com>
2024-04-11 22:00:20 +0300

    test 2 is done

--- cypress/e2e/ui-practice/checkbox-test.cy.js
+++ cypress/e2e/ui-practice/checkbox-test.cy.js
@@ -1,8 +1,21 @@
 describe("checkbox test", () => {
 
-    it("check is check 2 is default checked",() => {
+   beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/checkboxes');
+   })
 
-        cy.visit(" https://the-internet.herokuapp.com/checkboxes");
-        cy.get("input[type = \"checkbox\"")
+    it("check is check 2 is default checked",() => {        
+        cy.get("input[type = \"checkbox\"]").last().should("be.checked");
+        cy.get("input[type = \"checkbox\"]").last().uncheck();
+        cy.get("input[type = \"checkbox\"]").first().should("not.be.checked");
+        cy.get("input[type = \"checkbox\"]").last().should("not.be.checked");
+    })
+    
+    it("check the first checkbox, check both is checker", () => {
+       cy.wait(2000)
+       cy.get("input[type = \"checkbox\"]").first().check();
+       cy.get("input[type = \"checkbox\"]").first().should("be.checked");
+       cy.get("input[type = \"checkbox\"]").last().should("be.checked");
+   
     })
 })
